bb7231519e85550961b1efd7e1cdd3711ee39a19
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-04-17 13:01:12 -0400

    feat: finish flows rewrite

--- apps/www/lib/redirects.js
+++ apps/www/lib/redirects.js
@@ -2458,4 +2458,9 @@ module.exports = [
     source: '/docs/guides/auth/passwordless-login/auth-email-otp',
     destination: '/docs/guides/auth/auth-email-passwordless',
   },
+  {
+    permanent: true,
+    source: '/docs/guides/auth/native-mobile-login',
+    destination: '/docs/guides/auth/social-login',
+  },
 ]
